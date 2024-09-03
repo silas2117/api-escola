54b37dbfd2bbc61a79b81b0f4919e98244901407
silas2117 <82124043+silas2117@users.noreply.github.com>
2024-09-03 12:22:18 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8cdh+=x8b@#=g6!w0y+dckxw2kg-xi7lv-^$x5p(urifzosu_!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
